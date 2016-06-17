474e2e700cff13817b759f1f75218e2adfd82185
Benedikt Ritter <beneritter@gmail.com>
2016-06-17 09:42:52 +0200

    Initial commit

---
+++ src/main/java/de/codecentric/ocr/Ocr.java
@@ -0,0 +1,15 @@
+package de.codecentric.ocr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ocr {
+
+    public List<String> readAccountNumbers() {
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        new Ocr().readAccountNumbers();
+    }
+}
